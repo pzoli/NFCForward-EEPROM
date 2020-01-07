66a3fd42d2ec5ea837d84e1b000a2424d85445b1
pzoli <papp.zoltan@integrity.hu>
2020-01-07 11:05:37 +0100

    read server allow/denied result

--- nfcforward-eeprom.ino
+++ nfcforward-eeprom.ino
@@ -13,7 +13,6 @@ struct NetConfig
   uint8_t subnet[4];
   uint8_t gateway[4];
   uint8_t dnsserver[4];
-  uint8_t requestlen;
 } conf;
 
 byte* p = (byte*)(void*)&conf;
@@ -23,79 +22,65 @@ EthernetServer server(80);
 String serverAddress;
 boolean useDHCP = true;
 boolean configured = false;
-const boolean debugEth = false;
 #define SD_SS_PIN 4
 //#define RESETCONFIG true
 #define RST_PIN         9          // Configurable, see typical pin layout above
 #define SS_PIN          7         // Configurable, see typical pin layout above
 #define DEBUG true
-//#define WAITSERIAL true
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
-const byte default_id[4] = {192, 168, 1, 4};
-const uint8_t default_mac[6] = {0x46, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+String defRequest = "/log.php?readerId=%RID%&cardId=%CID%";
+String request = defRequest;
+
+const byte default_id[4] = {192, 168, 1, 14};
+const uint8_t default_mac[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 const uint8_t default_subnet[4] = {255, 255, 255, 0};
 const uint8_t default_gateway[4] = {192, 168, 1, 1};
 const uint8_t default_dnsserver[4] = {192, 168, 1, 1};
-const uint8_t default_serverip[4] = {192, 168, 1, 67};
-const uint8_t default_reqestlen = 36;
-const String default_request = "/log.php?readerId=%RID%&cardId=%CID%";
+
 char charArrayOfServerAddress[16];
 String stringOfMACAddress;
 
 void readConfig() {
   EEPROM_readAnything(0, conf);
-  for (int i = 0; i < conf.requestlen; i++) {
-    char c = (char)EEPROM.read(sizeof(conf) + i);
-    Serial.print(c);    
-  }
   Serial.println();
   sprintf(charArrayOfServerAddress, "%d.%d.%d.%d", conf.serverip[0], conf.serverip[1], conf.serverip[2], conf.serverip[3]);
   char charArrayOfMACAddress[17];
   sprintf(charArrayOfMACAddress, "%02X:%02X:%02X:%02X:%02X:%02X", conf.mac[0], conf.mac[1], conf.mac[2], conf.mac[3],conf.mac[4],conf.mac[5]);
   stringOfMACAddress = String((char*)charArrayOfMACAddress);
-#ifdef DEBUG
   printConfig(conf);
-#endif
 }
 
 void printIPToSerial(String ipname, IPAddress addr) {
   char ipno[16];
   sprintf(ipno, "%d.%d.%d.%d", addr[0], addr[1], addr[2], addr[3]);
   Serial.print(ipname);
-  Serial.print(":");
   Serial.print(ipno);
 }
 
 void printConfig(NetConfig oconf) {
   Serial.println("{");
-  Serial.print(F("state:'"));
+  Serial.print(F("'state':'"));
   Serial.print(oconf.state);
   Serial.println("',");
-  Serial.print(F("mac:'"));
+  Serial.print(F("'mac':'"));
   Serial.print(stringOfMACAddress);
   Serial.println("',");
-  Serial.print(F("useDHCP:'"));
+  Serial.print(F("'useDHCP':'"));
   Serial.print(oconf.useDHCP);
   Serial.println("',");
-  printIPToSerial(F("ip:'"), oconf.ip);
+  printIPToSerial(F("'ip':'"), oconf.ip);
   Serial.println("',");
-  printIPToSerial(F("subnet:'"), oconf.subnet);
+  printIPToSerial(F("'subnet':'"), oconf.subnet);
   Serial.println("',");
-  printIPToSerial(F("gateway:'"), oconf.gateway);
+  printIPToSerial(F("'gateway':'"), oconf.gateway);
   Serial.println("',");
-  printIPToSerial(F("dnsserver:'"), oconf.dnsserver);
+  printIPToSerial(F("'dnsserver':'"), oconf.dnsserver);
   Serial.println("',");
-  printIPToSerial(F("serverip:'"), oconf.serverip);
+  printIPToSerial(F("'serverip':'"), oconf.serverip);
   Serial.println("',");
-  Serial.print(F("requestLen:'"));
-  Serial.print(oconf.requestlen);
-  Serial.println("',");
-  Serial.print(F("request:'"));
-  for (int i = 0; i < conf.requestlen; i++) {
-    char c = (char)EEPROM.read(sizeof(conf) + i);
-    Serial.print(c);    
-  }
+  Serial.print(F("'request':'"));
+  Serial.print(request);
   Serial.println("'\n}");
 }
 
@@ -107,12 +92,7 @@ void resetConfig() {
   memcpy(conf.subnet, default_subnet, 4);
   memcpy(conf.gateway, default_gateway, 4);
   memcpy(conf.dnsserver, default_dnsserver, 4);
-  memcpy(conf.serverip, default_serverip, 4);
-  conf.requestlen = default_reqestlen;
   EEPROM_writeAnything(0, conf);
-  for (int i = 0; i < conf.requestlen; i++) {
-    EEPROM.write(sizeof(conf) + i, default_request.charAt(i));
-  }
 }
 
 void initRC522() {
@@ -127,6 +107,7 @@ void initEthernet() {
     Ethernet.begin(conf.mac);
   else
     Ethernet.begin(conf.mac, conf.ip, conf.dnsserver, conf.gateway, conf.subnet);
+  server.begin();
 #ifdef DEBUG
   Serial.print("reader ip: ");
   Serial.println(Ethernet.localIP());
@@ -150,14 +131,14 @@ void setup() {
   resetConfig();
 #endif
 
-#ifdef WAITSERIAL
+#ifdef DEBUG
   while (!Serial) {
     ;
   }
 #endif
   initPins();
   readConfig();
-  if (String(conf.state).startsWith("run") || debugEth) {
+  if (String(conf.state).startsWith("run")) {
     initEthernet();
     initRC522();
     configured = true;
@@ -176,22 +157,66 @@ EthernetClient webClient;
 String command;
 bool dataSent = false;
 bool firstLine = true;
+String webResult = "";
 
+void processRequest() {
+  #ifdef DEBUG
+      Serial.println();
+      Serial.println(F("webclient stop."));
+      Serial.println();
+      Serial.println(F("webresult:"));
+      Serial.println(webResult);
+#endif
+      int resultStartIdx = webResult.indexOf("{\"");
+      if (resultStartIdx > -1) {
+        int resultEndIdx = webResult.indexOf("\"}");
+        if (resultEndIdx > -1) {
+          webResult = webResult.substring(resultStartIdx,resultEndIdx+2);
+#ifdef DEBUG
+          Serial.println();
+          Serial.println(F("server result:"));
+          Serial.println(webResult);
+#endif
+          resultEndIdx = webResult.indexOf("\"}");
+          String allowed = webResult.substring(resultEndIdx-1,resultEndIdx);
+#ifdef DEBUG
+          Serial.println();
+          Serial.print(F("Allowed:"));
+          Serial.println(allowed);
+#endif    
+          if (allowed == "t") {
+#ifdef DEBUG
+            Serial.println();
+            Serial.println(F("Beengedem"));
+#endif    
+          } else {
+#ifdef DEBUG
+            Serial.println();
+            Serial.println(F("Kitiltom"));
+#endif                      
+          }
+        }        
+      }
+      webResult = "";
+}
 void loop() {
   if (configured) {
     if (webClient.available()) {
-      byte buff[64];
+      char buff[64];
       int cnt = webClient.read(buff, sizeof(buff));
+      for(int i=0;i<cnt;i++) {
+        webResult += buff[i];
+      }
 #ifdef DEBUG
       Serial.write(buff, cnt);
 #endif
-    }
+    } 
 
     if (!webClient.connected() && dataSent) {
       webClient.stop();
       dataSent = false;
+      processRequest();
     }
-
     // Look for new cards
     if ( mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
       String cardId;
@@ -205,21 +230,18 @@ void loop() {
 #ifdef DEBUG
       printIPToSerial(F("connect to"),conf.serverip);
       Serial.println();
+      Serial.print(F("cardid:"));
+      Serial.println(cardId);
 #endif
       if (webClient.connect(conf.serverip, 80)) {
         webClient.print(F("GET "));
-        char request[conf.requestlen];
-        for (int i = 0; i < conf.requestlen; i++) {
-          request[i]= (char)EEPROM.read(sizeof(conf) + i);    
-        }
-
-        //TODO Replace parameters
-        String req = String(request);
-        req.replace("%RID%",stringOfMACAddress);
-        req.replace("%CID%",cardId);
-        
-        Serial.print(F("Processed log request:"));
-        Serial.println(req);
+        request.replace(F("%RID%"),stringOfMACAddress);
+        request.replace(F("%CID%"),cardId);
+#ifdef DEBUG
+      Serial.println();
+      Serial.print(F("request:"));
+      Serial.println(request);
+#endif
         webClient.print(request);        
         webClient.println(F(" HTTP/1.1"));
         webClient.print(F("Host: "));
@@ -228,6 +250,7 @@ void loop() {
         webClient.println(F("Connection: close"));
         webClient.println();
         dataSent = true;
+        request = defRequest;
       } else {
 #ifdef DEBUG
         Serial.println(F("connection failed"));
@@ -240,10 +263,12 @@ void loop() {
     if (i < sizeof(conf)) {
       *p++ = Serial.read();
       i++;
+#ifdef DEBUG
       Serial.print("i(");
       Serial.print(sizeof(conf));
       Serial.print("):");
       Serial.println(i);
+#endif
     }
     if (i == sizeof(conf)) {
       p = (byte*)(void*)&conf;
@@ -251,15 +276,8 @@ void loop() {
       if (String(conf.state) == "configure") {
         strncpy(conf.state, "run      ", 10);
         EEPROM_writeAnything(0, conf);
-        String reqStr;
-        for (int i = 0; i < conf.requestlen; i++) {
-          char c = Serial.read();
-          Serial.print(c);
-          EEPROM.write(sizeof(conf) + i, c);
-        }
         readConfig();
         initEthernet();
-        configured = true;
       } else {
         Serial.print(F("unknow command ["));
         Serial.print(conf.state);
